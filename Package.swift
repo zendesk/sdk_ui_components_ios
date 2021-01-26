256ef0541ccf6b2a0f75981e7fe63b148f34e923
Sarp Solakoglu <ssolakoglu@zendesk.com>
2021-01-26 16:49:20 +0000

    Update README, CONTRIBUTING and LICENSE

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ZendeskSDKUIComponents",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ZendeskSDKUIComponents",
+            targets: [
+                "ZendeskSDKUIComponents"
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ZendeskSDKUIComponents",
+            path: "ZendeskSDKUIComponents.xcframework"
+        )
+    ]
+)
